64fd18cf73f735005ec2d68b85486dab554058af
Viviann Rydh <rydh.vivian@gmail.com>
2020-11-07 00:49:01 +0100

    finalversion

--- script.js
+++ script.js
@@ -5,74 +5,76 @@ let addButton=document.getElementById('add');
 
 
 add.addEventListener('click',()=>{
+
    if(userInput.value.length!=0){
-    let box=document.createElement('div');
-    box.className='input-group input-row';
-    
+       
+        let box=document.createElement('div');
+        box.className='input-group input-row';
+        
 
-    let input=document.createElement('input');
-    input.type='text';
-    input.id="to-do-input";
-    input.value=userInput.value;
-    input.disabled=true;
-  
+        let input=document.createElement('input');
+        input.type='text';
+        input.id="to-do-input";
+        input.value=userInput.value;
+        input.disabled=true;
     
-    let buttonsBox=document.createElement('div');
-    buttonsBox.className="to-do-buttons";
-    
+        
+        let buttonsBox=document.createElement('div');
+        buttonsBox.className="to-do-buttons";
+        
 
-    let changeButton=document.createElement("BUTTON")
-     changeButton.innerHTML = "Ändra";  
-     changeButton.className="change";               
-     
-     
-     let doneButton=document.createElement("BUTTON")
-     doneButton.innerHTML = "Färdig"; 
-     doneButton.className="done";                
-     
-    
-     let deleteButton=document.createElement("BUTTON")
-     deleteButton.innerHTML = "Radera";  
-     deleteButton.className="delete";    
-     
-     
-     buttonsBox.appendChild(changeButton);
-     buttonsBox.appendChild(deleteButton);
-     buttonsBox.appendChild(doneButton);
-     box.appendChild(input);
-     box.appendChild(buttonsBox);
-     document.getElementById('to-do-container').appendChild(box);
-     userInput.value='';
-     
-     changeButton.addEventListener('click',()=>{
-         input.disabled=false;
-         if(input.value.length!=0){
-            if(changeButton.innerText=='Ändra'){
-                changeButton.innerText='Spara';
+        let changeButton=document.createElement("BUTTON")
+        changeButton.innerHTML = "Ändra";  
+        changeButton.className="change";               
+        
+        
+        let doneButton=document.createElement("BUTTON")
+        doneButton.innerHTML = "Färdig"; 
+        doneButton.className="done";                
+        
+        
+        let deleteButton=document.createElement("BUTTON")
+        deleteButton.innerHTML = "Radera";  
+        deleteButton.className="delete";    
+        
+        
+        buttonsBox.appendChild(changeButton);
+        buttonsBox.appendChild(deleteButton);
+        buttonsBox.appendChild(doneButton);
+        box.appendChild(input);
+        box.appendChild(buttonsBox);
+        document.getElementById('to-do-container').appendChild(box);
+        userInput.value='';
+        
+        changeButton.addEventListener('click',()=>{
+            input.disabled=false;
+            if(input.value.length!=0){
+                if(changeButton.innerText=='Ändra'){
+                    changeButton.innerText='Spara';
+                } else {
+                    changeButton.innerText='Ändra';
+                    input.disabled=true;
+                }
             } else {
-                changeButton.innerText='Ändra';
-                input.disabled=true;
+                alert('Empty information can not be changed or saved!')
             }
-        } else {
-            alert('Empty information can not be changed or saved!')
-        }
-     })
+        })
 
 
-     doneButton.addEventListener('click', ()=>{
-        if(input.value.length!=0){
-            let finishedContainer=document.getElementById('finished-container');
-            buttonsBox.removeChild(doneButton);
-            finishedContainer.appendChild(box);
-            this.parentNode.parentNode.remove();
-        } else {
-            alert('The input information can not be empty');
-        }
-})  
+        doneButton.addEventListener('click', ()=>{
+            if(input.value.length!=0){
+                let finishedContainer=document.getElementById('finished-container');
+                buttonsBox.removeChild(doneButton);
+                finishedContainer.appendChild(box);
+                this.parentNode.parentNode.remove();
+            } else {
+                alert('The input information can not be empty');
+            }
+    })  
 
-    deleteButton.addEventListener('click',()=>{
-        deleteButton.parentNode.parentNode.remove();
-    })
+        deleteButton.addEventListener('click',()=>{
+            deleteButton.parentNode.parentNode.remove();
+        })
 } else {
     alert('The information box can not be empty!')
 }
